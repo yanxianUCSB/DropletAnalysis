f3aa8c4b0c8c0f45014359fbbc643bf7c599b3e7
yanxianUCSB <yanxian@umail.ucsb.edu>
2016-05-04 12:01:32 -0700

    binaryImage Thresholding for Test

--- dropletAnalysis.m
+++ dropletAnalysis.m
@@ -3,25 +3,25 @@ function path_root = dropletAnalysis(path_root)
 
 
 %% Set Default path of the head file
-% path_root = 'F:\Documents\Doc-Research\Doc-20150324-Tau Droplet\data\041316';
+path_root = 'F:\Documents\Doc-Research\Doc-20150324-Tau Droplet\data\test';
 
 if ~exist('path_root', 'var')
     path_root = uigetdir('C:/', 'Choose directory where the head.csv is');    %Choose directory containing TIFF files.
 end
 
 % Head Filename
-headfilename = 'head-P';
+headfilename = 'head';
 
 %%
 % processTifs([path_root, '\\raw']);
 
 %% Generate Size Distribution
 bwlabelpara = 4;  % lookup bwlabel.m
-Eccentricity = 1;
+Eccentricity = 0.9;
 thrd_adjust = -100; 
 numberOfBins = 50; % Or whatever you want.
 ifgroupon = 1;  % 1: average distribution of the regions
-minDiam = 2/0.322;
+minDiam = 0/0.322;
 maxDiam = 30/0.322;
 SCALE = 0.322 / 2;
 
@@ -37,26 +37,12 @@ sizeDist(path_root, ifgroupon, thrd_adjust, ...
     headfilename)
 %%
 
-% yLimMax = 50;
-% group.Salt = [0 10 20 30 50 100 150 500];
-% group.Tau = [15 50 80 160];
-% group.RNA = [50 70 150 240 480];
-% group.glycerol = [0.15];
-
-group.Salt = [0 30 50 100];
-group.Tau = [15 50 80 160];
-group.RNA = [50 150 240 480];
+group.Salt = [0 20 40 60 100];
+group.Tau = [160];
+group.RNA = [480];
 group.glycerol = [0.15];
-% 
-% compareSalt(path_root, group.Salt, group.Tau, group.RNA, group.glycerol, yLimMax);
-% compareTau(path_root, group.Salt, group.Tau, group.RNA, group.glycerol, yLimMax);
-% 
-% comparePolyA(path_root, group.Salt, group.Tau, group.RNA, group.glycerol, yLimMax);
-% compareSaltWithGlycerol(path_root, group.Salt, group.Tau, group.RNA, group.glycerol, yLimMax);
-% compareGlycerol(path_root, group.Salt, group.Tau, group.RNA, group.glycerol, yLimMax);
 
-
-Axis.xLim = [1.2 10];
+Axis.xLim = [0 10];
 Axis.yLim = [0 100];
 Axis.Units = {'mM', 'uM', 'ugmL', 'vv'};
 Instruction.col = [2 3 4 5];
@@ -65,9 +51,9 @@ DivCell = struct2cell(group);
 %%
 Instruction.comparision = [1 2 3 4];
 compareDim(path_root, DivCell, Instruction, Axis, headfilename);
-%
+%%
 Selection = [4 4];
-Axis.xLim = [3 10];
+Axis.xLim = [0 10];
 Axis.yLim = [0 60];
 compareDimThis(path_root, DivCell, Instruction, Axis, Selection, headfilename);
 %%
