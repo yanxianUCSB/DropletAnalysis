11be7cef05c91f606f3bb4d190d6ec47a5ffbc8c
yanxianUCSB <yanxian@umail.ucsb.edu>
2016-05-07 00:49:58 -0700

    Revert "Version042016"
    This reverts commit d2f1a93cd74827507bc087f95b888034c35af0c5.

--- dropletAnalysis.m
+++ dropletAnalysis.m
@@ -1,14 +1,14 @@
 function path_root = dropletAnalysis(path_root)
 
 %% Set Default path of the head file
-path_root = 'F:\Documents\Doc-Research\Doc-20150324-Tau Droplet\data\042016';
+% path_root = 'F:\Documents\Doc-Research\Doc-20150324-Tau Droplet\data\042016';
 
 if ~exist('path_root', 'var')
     path_root = uigetdir('C:/', 'Choose directory where the head.csv is');    %Choose directory containing TIFF files.
 end
 
 % Head Filename
-headfilename = 'head';
+headfilename = 'headVer05061602';
 
 %%
 % processTifs([path_root, '\\raw']);
@@ -23,34 +23,34 @@ minDiam = 2/0.322;
 maxDiam = 30/0.322;
 SCALE = 0.322 / 2;
 
-set(0,'DefaultFigureVisible','off');
+set(0,'DefaultFigureVisible','on');
 
-sizeDist(path_root, ifgroupon, thrd_adjust, ...
-    bwlabelpara,...
-    Eccentricity, ...
-    numberOfBins,...
-    SCALE, ...
-    minDiam, maxDiam,...
-    headfilename, 12)
+% sizeDist(path_root, ifgroupon, thrd_adjust, ...
+%     bwlabelpara,...
+%     Eccentricity, ...
+%     numberOfBins,...
+%     SCALE, ...
+%     minDiam, maxDiam,...
+%     headfilename)
 %%
 
-group.Salt = [0 10 20 30 50 100 150 500];
-group.Tau = [15 50 80 144 160];
-group.RNA = [50 63 70 150 240 480];
-group.Glry = [15];
+group.Salt = [0 0.1 1 10];
+group.SitTime = [5 10 20];
+group.Sample = [40];
+group.Trial = [1];
 
 Axis.xLim = [0 12];
 Axis.yLim = [0 150];
-Axis.Units = {'mM', 'uM', 'ug/mL', '%'};
+Axis.Units = {'mM', 'min', 'X', '#'};
 Axis.xLabel = 'Equivalent Diameter /\mum';
 Axis.yLabel = 'Count #';
 Instruction.col = [2 8 6 10];
-Instruction.names = {'NaCl', 'Tau', 'RNA', 'Glycerol'};
+Instruction.names = {'NaCl', 'SitTime', 'Obj', 'Trial'};
 DivCell = struct2cell(group);
 %%
 Instruction.comparision = [1 2 3 4];
-compareDim(path_root, DivCell, Instruction, Axis, headfilename);
-% Selection = [2 1 1];
-% compareDimThat(path_root, DivCell, Instruction, Axis, Selection, headfilename);
-% 
+% compareDim(path_root, DivCell, Instruction, Axis, headfilename);
+Selection = [2 1 1];
+compareDimThat(path_root, DivCell, Instruction, Axis, Selection, headfilename);
+
 end
