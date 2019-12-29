21afd4a0252edc42b6fd144954fa4153e149b4f0
yanxianUCSB <yanxian@umail.ucsb.edu>
2019-12-29 09:25:55 -0800

    ImageData added

--- ImageData.m
+++ ImageData.m
@@ -1,3 +1,16 @@
 classdef ImageData
+    properties
+        filepath = 'testin/2N4R-1/Pos0/img_000000000_Brightfield_000.tif';
+        A, map, transparency;
+    end
     
+    methods
+        function obj = ImageData(filepath)
+            obj.filepath = filepath;
+            [obj.A, obj.map, obj.transparency] = imread(obj.filepath);
+        end
+        function show(obj)
+            image(obj.A, 'CDataMapping', 'scaled');
+        end
+    end
 end
