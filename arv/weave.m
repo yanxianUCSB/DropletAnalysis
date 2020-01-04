776e2d42ad7b1e8ec78e0bedcf8350eb140cbe51
yanxianUCSB <yanxian@umail.ucsb.edu>
2020-01-03 19:01:41 -0800

    MAINT: Moved unused scripts to arv

---
+++ arv/weave.m
@@ -0,0 +1,8 @@
+function parms = weave(minDiam, maxDiam, ecc, circ, )
+% weave([1,2,3], [4,5,6])
+%      [[1,4], [1,5], [1,6]
+%       [2,4], [2,5], [2,6]
+%       [3,4], [3,5], [3,6]]
+coms = allcomb(varargin);
+
+end
